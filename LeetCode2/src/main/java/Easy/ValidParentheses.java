4038d1de5145c2fc909c71d30ab41252774fcc3f
newscout <d.ognev@unitedtraders.com>
2023-08-14 19:39:18 +0200

    fix ValidParentheses class

--- LeetCode2/src/main/java/Easy/ValidParentheses.java
+++ LeetCode2/src/main/java/Easy/ValidParentheses.java
@@ -1,34 +1,49 @@
 package Easy;
 
+import java.util.ArrayList;
+
 public class ValidParentheses {
     public static void main(String[] args) {
-        String s = "()[]{)";
+        String s = "({}[])[]";
         System.out.println(isValid(s));;
 
     }
 
     static boolean isValid(String s) {
-        boolean flag = true;
-        for(int i = 0; i < s.length(); i = i + 2){
-            if(s.charAt(i) == '('){
-                if(s.charAt(i + 1) != ')'){
-                    flag = false;
-                    break;
+        ArrayList<String> stack = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '('){
+                stack.add(String.valueOf(s.charAt(i)));
+            }
+            if(stack.isEmpty() &&
+                    (s.charAt(i) == '}' || s.charAt(i) == ')' || s.charAt(i) == ']')){
+                return false;
+            }
+            if(s.charAt(i) == '}') {
+                if (stack.get(stack.size() - 1).equals("{")) {
+                    stack.remove(stack.size() - 1);
+                } else {
+                    return false;
                 }
             }
-            if(s.charAt(i) == '['){
-                if(s.charAt(i + 1) != ']'){
-                    flag = false;
-                    break;
+            if(s.charAt(i) == ')') {
+                if (stack.get(stack.size() - 1).equals("(")) {
+                    stack.remove(stack.size() - 1);
+                } else {
+                    return false;
                 }
             }
-            if(s.charAt(i) == '{'){
-                if(s.charAt(i + 1) != '}'){
-                    flag = false;
-                    break;
+            if(s.charAt(i) == ']') {
+                if (stack.get(stack.size() - 1).equals("[")) {
+                    stack.remove(stack.size() - 1);
+                } else {
+                    return false;
                 }
+            }
+            if(!stack.isEmpty() && i == s.length() - 1){
+                return false;
             }
         }
-        return flag;
+        return true;
     }
 }
