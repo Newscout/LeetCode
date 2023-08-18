55c01c771bf5326791edfeaeda22615a89e4c1f1
newscout <d.ognev@unitedtraders.com>
2023-08-18 17:14:27 +0200

    fix RemoveDuplicatesFromSortedArray26

--- LeetCode2/src/main/java/Easy/RemoveDuplicatesFromSortedArray26.java
+++ LeetCode2/src/main/java/Easy/RemoveDuplicatesFromSortedArray26.java
@@ -11,8 +11,8 @@ import java.util.Set;
 public class RemoveDuplicatesFromSortedArray26 {
     public static void main(String[] args) {
 
-        int [] arr = {6, 3, 7, 2, 7, 5 , 6, 8, 8, 9, 10, 3, 1};
-//        int [] arr = {1, 1, 2};
+//        int [] arr = {6, 3, 7, 2, 7, 5 , 6, 8, 8, 9, 10, 3, 1};
+        int [] arr = {1, 1, 2};
 //        int [] arr = {0,0,1,1,1,2,2,3,3,4};
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -23,12 +23,19 @@ public class RemoveDuplicatesFromSortedArray26 {
 
     }
     static int newArray(int[] nums){
-        Set<String> set = new HashSet<>();
-        set.add(String.valueOf(nums[0]));
+        Arrays.sort(nums);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
         for (int i = 1; i < nums.length; i++) {
-            set.add(String.valueOf(nums[i]));
+            if(nums[i] != list.get(list.size() - 1)){
+                list.add(nums[i]);
+            }
         }
-        System.out.println(set);
-        return set.size();
+        int [] newNums = new int[list.size()];
+        for (int i = 0; i < newNums.length; i++) {
+            newNums[i] = list.get(i);
+        }
+        nums = newNums;
+        return nums.length;
     }
 }
