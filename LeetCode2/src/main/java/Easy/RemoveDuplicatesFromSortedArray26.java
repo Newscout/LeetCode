ca1f20d233c41d9e881136813805494eaf64c8da
newscout <d.ognev@unitedtraders.com>
2023-08-21 19:23:29 +0200

    remove ArrayList and use only Array for  RemoveDuplicatesFromSortedArray26

--- LeetCode2/src/main/java/Easy/RemoveDuplicatesFromSortedArray26.java
+++ LeetCode2/src/main/java/Easy/RemoveDuplicatesFromSortedArray26.java
@@ -11,8 +11,8 @@ import java.util.Set;
 public class RemoveDuplicatesFromSortedArray26 {
     public static void main(String[] args) {
 
-//        int [] arr = {6, 3, 7, 2, 7, 5 , 6, 8, 8, 9, 10, 3, 1};
-        int [] arr = {1, 1, 2};
+        int [] arr = {6, 3, 7, 2, 7, 5 , 6, 8, 8, 9, 10, 3, 1};
+//        int [] arr = {1, 1, 2};
 //        int [] arr = {0,0,1,1,1,2,2,3,3,4};
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -24,18 +24,30 @@ public class RemoveDuplicatesFromSortedArray26 {
     }
     static int newArray(int[] nums){
         Arrays.sort(nums);
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(nums[0]);
+        System.out.println();
+        int [] nonDuplicateNums = new int[nums.length];
+        nonDuplicateNums[0] = nums[0];
+        int indexForAddElement = 1;
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i] != list.get(list.size() - 1)){
-                list.add(nums[i]);
+            if(nums[i] != nums[i - 1]){
+                nonDuplicateNums[indexForAddElement] = nums[i];
+                indexForAddElement += 1;
             }
         }
-        int [] newNums = new int[list.size()];
-        for (int i = 0; i < newNums.length; i++) {
-            newNums[i] = list.get(i);
+        int count = 1;
+        for (int i = 0; i < nonDuplicateNums.length; i++) {
+            nums[i] = nonDuplicateNums[i];
+            if(i != 0 && nonDuplicateNums[i] > nonDuplicateNums[i - 1]){
+                count += 1;
+            }
         }
-        nums = newNums;
-        return nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+
+        return count;
     }
 }
